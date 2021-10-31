395bc25bb80c45fe8dc9f9e5de07bb7699bb93d9
davidhl01 <93193043+davidhl01@users.noreply.github.com>
2021-10-31 18:19:10 +0100

    Delete Tema 2 directory

--- Tema 2/Ejercicios/Worksheet1/Ejercicio-1.js
+++
@@ -1,8 +0,0 @@
-fecha = new Date();
-
-document.write('Año:' + fecha.getFullYear() + '<br>');
-document.write('Mes:' + fecha.getMonth() + '<br>');
-document.write('Fecha:' + fecha.getDate() + '<br>');
-document.write('Horas:' + fecha.getHours() + ' <br >');
-document.write('Minutos:' + fecha.getMinutes() + '<br>');
-document.write('Segundos:' + fecha.getSeconds() + '<br>');
--- Tema 2/Ejercicios/Worksheet1/Ejercicio-4.js
+++
@@ -1,3 +0,0 @@
-setTimeout(function() {
-    alert(location.href = "https://www.google.es/");
-}, 3000);
