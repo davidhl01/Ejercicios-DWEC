1ed7d3ce1891e39d3fcf857288f14ecf4b8e2fd7
David Hernández López <93193043+davidhl01@users.noreply.github.com>
2022-01-10 10:33:59 +0100

    Add files via upload

---
+++ Tema 7/Worksheet 1/Ejercicio 8/Index.js
@@ -0,0 +1,26 @@
+//a)
+$('div > p:eq(0)').css("font-size",10);
+$('div > p:eq(1)').css("font-size",20);
+$('div > p:eq(2)').css("font-size",30);
+
+//b)
+$('h2:eq(0)').click(function () { 
+    $('p:eq(0)').hide(); 
+});
+$('h2:eq(1)').click(function () { 
+    $('p:eq(1)').hide(); 
+});
+$('h2:eq(2)').click(function () { 
+    $('p:eq(2)').hide(); 
+});
+
+//c) y d)
+for (let i = 0; i < 3; i++) {
+    $(`div > p:eq(${i})`).hover(function(){
+        $(`div > p:eq(${i})`).css('background-color','red');
+    });
+
+    $(`div > p:eq(${i})`).mouseout(function(){
+        $(`div > p:eq(${i})`).css('background-color','inherit');
+    });
+}
